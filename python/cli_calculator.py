186670841898b1fbdfcfa564fd2f8adc28663f2b
Christopher Wheeler <wheelerchristopherg@gmail.com>
2022-07-09 13:40:22 -0500

    can now resolve variables given an environment during evaluation instead of parsing

--- python/cli_calculator.py
+++ python/cli_calculator.py
@@ -13,6 +13,32 @@ class Token:
     return self.__str__()
 
 
+class OpenParen(Token):
+  weight = 0
+  def __init__(self):
+    self.value = "("
+
+
+class CloseParen(Token):
+  weight = 0
+  def __init__(self):
+    self.value = ")"
+
+
+class Variable(Token):
+  weight = 0
+  def __init__(self, value):
+    self.value = value
+
+  def evaluate(self, env):
+    if self.value in env:
+      result = env[self.value]
+      if isinstance(result, BinaryTree):
+        return result.evaluate(env)
+      return result
+    raise Exception(f"Invalid Variable: {self.value}")
+
+
 class Number(Token):
   weight = 0
   def __init__(self, value):
@@ -79,25 +105,58 @@ class BinaryTree:
   def _check_valid_number_token(self):
     if isinstance(self.token, Number): 
       if self.left or self.right:
-        raise Exception("Invalid Expression")
+        raise Exception("Invalid Tree")
       return self.token.evaluate()
+    return None
+
+  def _check_valid_var_token(self, env):
+    if isinstance(self.token, Variable): 
+      if self.left or self.right:
+        raise Exception("Invalid Tree")
+      return self.token.evaluate(env)
     return None
 
   def _check_valid_operation(self):
     if not (self.left and self.right and self.token):
       raise Exception(f"Invalid Operation: {self.token}({self.left}, {self.right})")
 
-  def evaluate(self):
+  def evaluate(self, env):
     if (valid_number := self._check_valid_number_token()) != None:
       return valid_number
+
+    if (resolved_var := self._check_valid_var_token(env)) != None:
+        return resolved_var
+
     self._check_valid_operation()
-    return self.token.evaluate(self.left.evaluate(), self.right.evaluate())
+    return self.token.evaluate(self.left.evaluate(env), self.right.evaluate(env))
 
 
-def token_factory(text, env):
-  if text in env:
-    return Number(env[text])
-  elif Number.is_number(text):
+class TreeBuilder:
+  def __init__(self, tokens):
+    self.sub_tree_map = {}
+    self.tokens = tokens
+
+  def find_root(self, tokens):
+    root_index = 0
+    for i, t in enumerate(tokens):
+      if t.weight >= tokens[root_index].weight:
+        root_index = i
+    return root_index
+  
+  
+  def build_tree(self, tokens):
+    if not tokens:
+      raise Exception("Invalid Expression")
+    elif len(tokens) == 1:
+      return BinaryTree(tokens[0])
+    root_index = find_root(tokens)
+    left_node = build_tree(tokens[:root_index])
+    right_node = build_tree(tokens[root_index+1:])
+    return BinaryTree(tokens[root_index], left_node, right_node)
+
+
+def token_factory(text):
+  if Number.is_number(text):
     return Number(text)
   elif text == "*":
     return Multiply()
@@ -107,13 +166,19 @@ def token_factory(text, env):
     return Add()
   elif text == "-":
     return Subtract()
+  elif text == "(":
+    return OpenParen()
+  elif text == ")":
+    return CloseParen()
+  elif text != "":
+    return Variable(text)
   raise Exception("Invalid token")
 
 
-def parse_tokens(expression, env):
+def parse_tokens(expression):
   tokens = list()
   for t in expression.split():
-    tokens.append(token_factory(t, env))
+    tokens.append(token_factory(t))
   return tokens
 
 
@@ -136,18 +201,8 @@ def build_tree(tokens):
   return BinaryTree(tokens[root_index], left_node, right_node)
 
 
-def evaluate_tree(tree):
-  if not tree:
-    raise Exception("Invalid expression")
-  if isinstance(tree.token, Number):
-    if tree.left or tree.right:
-      raise Exception("Invalid expression")
-    return tree.token.get_decimal()
-  return tree.evaluate()
-
-
 def build_env_from_history(history):
-  return {f"x{i}" : f"{value}" for i, value in enumerate(history)}  
+  return {f"x{i}" : value for i, value in enumerate(history)}  
 
 
 def main():
@@ -160,9 +215,9 @@ def main():
     #expression = "123.321 + 112 - 759 * 7 / 3"
     env = build_env_from_history(history)
     try:
-      tokens = parse_tokens(expression, env)
+      tokens = parse_tokens(expression)
       tree = build_tree(tokens)
-      result = tree.evaluate()
+      result = tree.evaluate(env)
     except Exception as e:
       print(e)
       continue
