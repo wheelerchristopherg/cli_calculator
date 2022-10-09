3212b07dc22dc32b27d7df85e0c2e6eeef349e59
Christopher Wheeler <wheelerchristopherg@gmail.com>
2022-10-08 23:39:58 -0500

    add read_line function and main loop for accepting expressions

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,35 @@
 mod tokens;
+use std::io;
 use tokens::Token;
 
 fn main() {
-    let s = "    \t";
-    let t: Token = Token::from(s);
-    println!("{} = {:?}", s, t);
+    loop {
+        println!("Enter Expression:");
+        match read_line() {
+            Ok(expression) => {
+                let exp = expression.trim();
+                if exp == "q" {
+                    break;
+                }
+                let str_tokens = exp.split(" ");
+                let mut parsed_tokens: Vec<Token> = Vec::new();
+                for t in str_tokens {
+                    parsed_tokens.push(Token::from(t))
+                }
+                println!("Tokens: {:?}", parsed_tokens)
+            }
+            Err(_) => {
+                println!("Oops! Something went wrong!");
+            }
+        }
+    }
+}
+
+fn read_line() -> Result<String, io::Error> {
+    let mut user_input = String::new();
+    let stdin = io::stdin();
+    match stdin.read_line(&mut user_input) {
+        Ok(_) => Ok(user_input),
+        Err(e) => Err(e),
+    }
 }
