8e84d1a23005408a905d2773afbe44cb1a92fa52
Christopher Wheeler <wheelerchristopherg@gmail.com>
2023-04-29 21:57:10 -0500

    finally added parenthetical expressions

--- rust/src/ast.rs
+++ rust/src/ast.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests;
 
-use crate::tokens::{Num, Op, Token};
+use crate::tokens::{Num, Op, ParenType, Token};
 use std::{boxed::Box, collections::HashMap, fmt::Display};
 
 #[derive(Debug)]
@@ -9,6 +9,12 @@ pub struct AST {
     value: Token,
     left: Option<Box<AST>>,
     right: Option<Box<AST>>,
+}
+
+#[derive(Debug, Clone)]
+struct WeightedToken {
+    token: Token,
+    weight: i32,
 }
 
 impl AST {
@@ -85,24 +91,24 @@ impl AST {
         }
     }
 
-    fn get_token_weight(token: &Token) -> Option<i32> {
-        match token {
-            Token::Paren(_) => Some(0),
-            Token::Number(_) => Some(0),
-            Token::Variable(_) => Some(0),
+    fn get_token_weight(token: &WeightedToken) -> Option<i32> {
+        match &token.token {
+            Token::Paren(_) => Some(token.weight),
+            Token::Number(_) => Some(token.weight),
+            Token::Variable(_) => Some(token.weight),
             Token::Operator(oper) => match oper {
-                Op::Add => Some(2),
-                Op::Sub => Some(2),
-                Op::Mult => Some(1),
-                Op::Div => Some(1),
+                Op::Add => Some(token.weight + 2),
+                Op::Sub => Some(token.weight + 2),
+                Op::Mult => Some(token.weight + 1),
+                Op::Div => Some(token.weight + 1),
             },
             _ => None,
         }
     }
 
-    fn find_root_index(tokens: &[Token]) -> usize {
+    fn find_root_index(tokens: &[WeightedToken]) -> usize {
         let mut root_index = 0;
-        let mut weight = -1;
+        let mut weight = i32::MIN;
         for (i, t) in tokens.iter().enumerate() {
             if let Some(x) = Self::get_token_weight(t) {
                 if x >= weight {
@@ -115,11 +121,16 @@ impl AST {
     }
 
     pub fn build_tree(tokens: &[Token]) -> Result<Box<Self>, String> {
+        let weighted_tokens: &[WeightedToken] = &Self::adjust_weights(tokens).unwrap();
+        Self::build_tree_weighted(weighted_tokens)
+    }
+
+    fn build_tree_weighted(tokens: &[WeightedToken]) -> Result<Box<Self>, String> {
         let mut end = tokens.len();
         if end == 0 {
             Err("No tokens to parse")?
         }
-        if let Some(e) = tokens.iter().position(|x| x == &Token::EOL) {
+        if let Some(e) = tokens.iter().position(|x| x.token == Token::EOL) {
             end = e;
         }
 
@@ -129,18 +140,48 @@ impl AST {
 
         if root_index > 0 {
             left = Some(
-                Self::build_tree(&tokens[..root_index])
+                Self::build_tree_weighted(&tokens[..root_index])
                     .map_err(|_| "Invalid Expression".to_string())?,
             )
         }
         if root_index > 0 && root_index < tokens.len() - 1 {
             right = Some(
-                Self::build_tree(&tokens[(root_index + 1)..end])
+                Self::build_tree_weighted(&tokens[(root_index + 1)..end])
                     .map_err(|_| "Invalid Expression".to_string())?,
             )
         }
 
-        Ok(Self::new(tokens[root_index].clone(), left, right))
+        Ok(Self::new(tokens[root_index].token.clone(), left, right))
+    }
+
+    fn adjust_weights(tokens: &[Token]) -> Result<Vec<WeightedToken>, String> {
+        let mut level = 0;
+        let mut weighted_tokens: Vec<WeightedToken> = vec![];
+        for token in tokens.iter() {
+            let weighted_token: Option<WeightedToken> = match token {
+                Token::Paren(ParenType::OpenParen) => {
+                    level -= 10;
+                    None
+                }
+                Token::Paren(ParenType::CloseParen) => {
+                    level += 10;
+                    None
+                }
+                _ => Some(WeightedToken {
+                    token: token.clone(),
+                    weight: level,
+                }),
+            };
+            if let Some(t) = weighted_token {
+                weighted_tokens.push(t.clone());
+            }
+        }
+
+        if level != 0 {
+            Err("Mismatched parenthases".to_string())
+        } else {
+            Ok(weighted_tokens)
+        }
     }
 }
 
